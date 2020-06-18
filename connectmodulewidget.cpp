69ab01a08f651687f0f23ab414ba66410fdbe602
alex13sh <alex13sh@localhost.localdomain>
2020-06-18 10:07:44 +0300

    Реализовал функции connect и add

--- connectmodulewidget.cpp
+++ connectmodulewidget.cpp
@@ -1,14 +1,48 @@
 #include "connectmodulewidget.h"
 #include "ui_connectmodulewidget.h"
 
+#include <QModbusTcpClient>
+#include <QModbusRtuSerialMaster>
+#include <QTimer>
+#include <QDebug>
+
 ConnectModuleWidget::ConnectModuleWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ConnectModuleWidget)
 {
     ui->setupUi(this);
+    ui->txtDeviceAddress->setText("/dev/ttyACM0");
 }
 
 ConnectModuleWidget::~ConnectModuleWidget()
 {
     delete ui;
+}
+
+void ConnectModuleWidget::on_pbConnect_clicked() {
+    if(modbusDevice) delete modbusDevice;
+    if(ui->cmbType->currentText() == "RTU")
+        modbusDevice = new QModbusRtuSerialMaster(this);
+    else modbusDevice = new QModbusTcpClient(this);
+
+    modbusDevice->setConnectionParameter(QModbusDevice::SerialPortNameParameter,
+        ui->txtDeviceAddress->text());
+
+    modbusDevice->setTimeout(1000);
+    modbusDevice->setNumberOfRetries(3);
+    if (!modbusDevice->connectDevice())
+        qDebug()<<tr("Connect failed: ") + modbusDevice->errorString();
+    else qDebug()<<"Connected!";
+}
+
+void ConnectModuleWidget::on_pbAdd_clicked() {
+    if(!modbusDevice) return;
+    auto name = ui->txtName->text();
+    if(mapDevices.contains(name))
+        qDebug()<<"Такое имя устройства уже существует";
+    else{
+        mapDevices[name] = modbusDevice;
+        modbusDevice = nullptr;
+        qDebug()<<"Устройство"<<name<<"добавленно";
+    }
 }
--- connectmodulewidget.h
+++ connectmodulewidget.h
@@ -2,6 +2,11 @@
 #define CONNECTMODULEWIDGET_H
 
 #include <QWidget>
+#include <QMap>
+
+class QModbusClient;
+class QModbusReply;
+class QTimer;
 
 namespace Ui {
 class ConnectModuleWidget;
@@ -15,8 +20,18 @@ public:
     explicit ConnectModuleWidget(QWidget *parent = nullptr);
     ~ConnectModuleWidget();
 
+private slots:
+    void on_pbConnect_clicked();
+
 private:
     Ui::ConnectModuleWidget *ui;
+
+    using ValuesType = QVector<quint16>;
+
+    QModbusReply *lastRequest = nullptr;
+    QModbusClient *modbusDevice = nullptr;
+    QTimer *tim_value=nullptr;
+    QMap<QString, QModbusClient*> mapDevices;
 };
 
 #endif // CONNECTMODULEWIDGET_H
