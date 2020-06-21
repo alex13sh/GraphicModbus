f69ac406e41455f7e6a4f4b88af3dcc0f7694e14
alex13sh <alex97sh@yandex.ru>
2020-06-21 17:41:57 +0300

    Update struct Sensor

--- structs.h
+++ structs.h
@@ -31,25 +31,32 @@ typedef QMap<QString, ModbusDevice> MapDevices_;
 
 struct Sensor{
     QString name = "None";
-    enum SensorType{
+    enum SensorType_{
         None=0,
         Volt_1=14,
         Resister=39
-    } type = None;
+    } type_ = None;
+    enum SensorType{
+        Other=0,
+        Температура,
+        Давление
+    } type = Other;
     const ModbusDevice *module=nullptr;
+    quint16 address;
 
     float fvalue;
     int ivalue;
 
     Sensor()
-        : name("None"), type(None)
+        : name("None")
     {}
-    Sensor(const QString &name)
-        : name(name)
+    Sensor(const QString &name, quint16 address)
+        : name(name), address(address)
     {}
 
-    void setModule(const ModbusDevice *module);
-    void setType(SensorType type);
+    void setModule(const ModbusDevice *module){this->module=module;}
+    void setType_(SensorType_ type){type_=type;}
 };
+typedef QMap<QString, Sensor>* MapSensors;
 
 #endif // STRUCTS_H
