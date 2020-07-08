9780f5cbe4d1d4a95bac3ec3811757a5634d0e88
alex13sh <alex97sh@yandex.ru>
2020-07-08 16:41:50 +0300

    Update test into mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -35,19 +35,29 @@ MainWindow::~MainWindow()
 #include <QDebug>
 
 void MainWindow::init_test_device() {
+    qDebug()<<"init_test_device";
+    int bp_cnt=1;
     auto dev_analog = new ModbusDevice_Analog("Input Analog", this);
+    qDebug()<<"bp_"<<bp_cnt++;
     mapDevices->insert(dev_analog->name(), dev_analog);
     dev_analog->connectRTU("/dev/ttyACM0");
-
+    qDebug()<<"bp_"<<bp_cnt++;
 //    if(dev_analog->isConnected()){
         dev_analog->setADC(false);
         auto sens_analog = static_cast<ModbusSensor_Analog *>(dev_analog->createSensor(1, "Термо 1"));
         sens_analog->setInterval(800);
         sens_analog->setType(ModbusSensor_Analog::Pt_100);
-
+        sens_analog = static_cast<ModbusSensor_Analog *>(dev_analog->createSensor(2, "Термо 2"));
+        sens_analog->setInterval(800);
+        sens_analog->setType(ModbusSensor_Analog::Pt_100);
+qDebug()<<"bp_"<<bp_cnt++;
 //        sens_analog = static_cast<ModbusSensor_Analog *>(dev_analog->createSensor(5, "Пирометр"));
 //        sens_analog->setInterval(1000);
 //        sens_analog->setType(ModbusSensor_Analog::Amper_4_20);
+//        for(int i=3; i<8; ++i){
+//            sens_analog = static_cast<ModbusSensor_Analog *>(dev_analog->createSensor(i, "Пирометр"));
+//            sens_analog->setType(ModbusSensor_Analog::None);
+//        }
 //    }
 
 //    auto dev_digital = new ModbusDevice_IODigital("Input\Output Digital", this);
